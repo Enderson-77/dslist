7ae570d8ad2e3fdae96a2a901c7d0c9fffe3e5b1
Cassio Enderson <cassioenderson@gmail.com>
2025-01-21 22:19:38 -0300

    SQL, projection, get games by list

--- src/main/java/com/cassioenderson/dslist/dto/GameMinDTO.java
+++ src/main/java/com/cassioenderson/dslist/dto/GameMinDTO.java
@@ -1,44 +1,48 @@
 package com.cassioenderson.dslist.dto;
 
 import com.cassioenderson.dslist.entities.Game;
+import com.cassioenderson.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
-    private Long id;
-    private String title;
-    private Integer year;
-    private String imgUrl;
-    private String shortDescription;
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
 
-    public GameMinDTO() {
-    }
+	public GameMinDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
 
-    public GameMinDTO(Game entity) {
-        id = entity.getId();
-        title = entity.getTitle();
-        year = entity.getYear();
-        imgUrl = entity.getImgUrl();
-        shortDescription = entity.getShortDescription();
-    }
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public Integer getYear() {
+		return year;
+	}
 
-    public Integer getYear() {
-        return year;
-    }
+	public String getImgUrl() {
+		return imgUrl;
+	}
 
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
+	public String getShortDescription() {
+		return shortDescription;
+	}
 }
--- src/main/java/com/cassioenderson/dslist/services/GameService.java
+++ src/main/java/com/cassioenderson/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.cassioenderson.dslist.dto.GameDTO;
 import com.cassioenderson.dslist.dto.GameMinDTO;
 import com.cassioenderson.dslist.entities.Game;
+import com.cassioenderson.dslist.projections.GameMinProjection;
 import com.cassioenderson.dslist.repositories.GameRepository;
 
 @Service
@@ -30,5 +31,11 @@ public class GameService {
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
+
+    @Transactional(readOnly = true)
+	public List<GameMinDTO> findByGameList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		return games.stream().map(GameMinDTO::new).toList();
+	}
 
 }
